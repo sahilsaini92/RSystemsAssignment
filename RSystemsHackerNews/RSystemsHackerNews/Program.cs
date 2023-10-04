cff3bd2bcb0b3b69cfeab5362d5d9e98884bd17f
sahil saini <sahilsaini92@gmail.com>
2023-10-04 23:23:09 +0530

    APi version

--- RSystemsHackerNews/RSystemsHackerNews/Program.cs
+++ RSystemsHackerNews/RSystemsHackerNews/Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Stories",
+        Version = "v1"
+    });
+});
 
 builder.Services.AddCors(options =>
 {
